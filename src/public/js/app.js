5b1efe7065d1e4f3b2cffb7d251d45244b8fbdbf
MinJeung-Kim <focso5@gmail.com>
2024-05-16 12:48:52 +0900

    Add: set webRtc:

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,91 +1,45 @@
-const socket = io(); // 실행중인 server의 socket을 찾음.
+const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-// 1. room dom 숨기기
-room.hidden = true;
+let myStream; // stream = 비디오와 오디오가 결합됨.
+let muted = false; // 오디오 켜진상태
+let cameraOff = false; // 비디오 켜진상태
 
-let roomName;
-
-function addMessae(message) {
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value
-    // 8. input.value를 new_message라는 이벤트로 server에 전송.
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessae(`You: ${value}`);
-    });
-    input.value = "";
-}
-
-function handleNicknameSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname", input.value);
-
-}
-
-function showRoom() {
-    // 3. back-end에서 실행 시킨 함수 동작.
-    welcome.hidden = true;
-    room.hidden = false;
-    // 5. 저장된 room name을 title로 넣어줌.
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
-    const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = form.querySelector("input");
-    // 2. 방 이름(enter_room)을 가지고 방에 입장하면 
-    // showRoom 함수 back-end에 보냄
-    socket.emit("enter_room", input.value, showRoom);
-    // 4. room name 변수에 저장.
-    roomName = input.value;
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    // 6. 누군가가 입장했다는 message를 보냄.
-    addMessae(`${user} arrived!`);
-});
-
-socket.on("bye", (left, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    // 7. 누군가가 나갔다는 message를 보냄.
-    addMessae(`${left} left ㅠㅠ`);
-});
-
-socket.on("new_message", addMessae);
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-
-    if (rooms.length === 0) {
-        return;
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            // 오디오와 비디오를 얻어오겠다.
+            audio: true,
+            video: true
+        });
+        myStream.srcObject = myStream;
+        console.log(myStream);
+    } catch (err) {
+        console.log(err);
     }
+}
+function handleMuteClick() {
+    // 음속어가 되어 있지 않다면
+    if (!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraClick() {
+    if (cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
 
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-});
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import { SocketIO } from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,100 +11,11 @@ app.set("views", __dirname + "/public/views");
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    }
-});
-
-instrument(wsServer, {
-    auth: false,
-});
-
-// 1. connection 받을 준비
-wsServer.on("connection", socket => {
-
-    // public rooms을 주는 function.
-    function publicRooms() {
-        // wsServer.socket.adapter로 부터 sids와 rooms를 가져옴.
-        const { sockets: { adapter: { sids, rooms } } } = wsServer;
-        // const sids = wsServer.socket.adapter.sids;
-        // const rooms = wsServer.socket.adapter.rooms;
-        const publicRooms = [];
-        rooms.forEach((_, key) => {
-            if (sids.get(key) === undefined) {
-                publicRooms.push(key);
-            };
-        });
-        return publicRooms;
-    }
-
-    function countRoom(roomName) {
-        return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-    }
-
-    socket["nickname"] = "Anon";
-    socket.on("enter_room", (roomName, done) => {
-        // 2. room에 참가
-        socket.join(roomName);
-        // 3. front-end fn 실행
-        done();
-        // 4. roomName에 있는 모든 사람들에게 welcome event를 emit.
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-
-        // 모든 socket에 보냄.
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    // 5. roomName에 있는 모든 사람들에게 disconnecting event를 실행해서 
-    // "bye"라는 메세지 전송.
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach(room =>
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
-    });
-
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-
-    // nickname event가 발생하면 nickname를 가져와서 socket에 저장.
-    socket.on("nickname", nickname => socket["nickname"] = nickname);
-});
-/** 
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
- 
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
- 
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload.toString()}`))
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-            default:
-                break;
-        }
-    });
-});
-*/
+const wsServer = SocketIO(httpServer);
 
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
 
